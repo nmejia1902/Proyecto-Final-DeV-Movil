126c2616b14cec5da1506cc3dd4b3eb2ec41237f
nmejia1902 <159928835+nmejia1902@users.noreply.github.com>
2026-03-11 21:11:39 -0600

    Delete pedidosapp directory

--- pedidosapp/backend/routes/pedidos.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pedidosController = require('../controllers/pedidosController');
-
-router.get('/', pedidosController.obtenerPedidos);
-
-router.post('/', pedidosController.crearPedido);
-
-module.exports = router;
